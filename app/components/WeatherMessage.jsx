ddc6606e657a4a0941723d169970c71378e1610c
Wiched <lubo@jordanov.info>
2017-05-10 11:21:29 +0300

    in celsius

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3 className="text-center">It's it Math.floor({temp})°C in {location}.</h3>
+    <h3 className="text-center">It's it {temp}°C in {location}.</h3>
   )
 };
 
