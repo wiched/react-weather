dd554ea75a4e887cd985fb70431cb9da9452c6d2
Wiched <lubo@jordanov.info>
2017-05-10 10:04:55 +0300

    major fixes

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,8 @@ var About = require('About');
 var Examples = require('Examples');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
+require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!applicationStyles');
 $(document).foundation();
 
 ReactDOM.render(
