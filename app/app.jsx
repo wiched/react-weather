c45726c27d848e82b1678b6fdd025540035b459c
Wiched <lubo@jordanov.info>
2017-05-10 10:56:02 +0300

    fixing modal error

--- app/app.jsx
+++ app/app.jsx
@@ -8,7 +8,7 @@ var Examples = require('Examples');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 $(document).foundation();
 
 ReactDOM.render(
